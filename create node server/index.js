906e2bf11d68d7628ede302bf419274a0514c507
veejask-41 <sajeevk2001@gmail.com>
2023-09-19 16:34:36 +0530

    Json response

--- create node server/index.js
+++ create node server/index.js
@@ -1,8 +1,13 @@
 const http = require("node:http");
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {"Content-Type":"text/plain"});
-  res.end("hi sajeev!");
+  const data = {
+    name: "sajeev",
+    age: 14
+  }
+  
+  res.writeHead(200, {"Content-Type":"application/json"});
+  res.end(JSON.stringify(data));
 //   console.log(req);
 });
 
