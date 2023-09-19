3f2f650b9454815f7b3aabfa7a6bae3d1e39571b
veejask-41 <sajeevk2001@gmail.com>
2023-09-19 16:54:05 +0530

    html response

--- create node server/index.js
+++ create node server/index.js
@@ -1,14 +1,19 @@
 const http = require("node:http");
+const fs = require("node:fs");
 
 const server = http.createServer((req, res) => {
-  const data = {
-    name: "sajeev",
-    age: 14
-  }
-  
-  res.writeHead(200, {"Content-Type":"application/json"});
-  res.end(JSON.stringify(data));
-//   console.log(req);
+  // const data = {
+  //   name: "sajeev",
+  //   age: 14
+  // }
+
+  res.writeHead(200, { "Content-Type": "text/html" });
+  // res.end(JSON.stringify(data));
+  // res.end("<h1>Hello World!</h1>")
+  // const html = fs.readFileSync("./index.html", "utf-8");
+  // res.end(html);
+  fs.createReadStream(__dirname+"/index.html").pipe(res);
+  //   console.log(req);
 });
 
 server.listen(1234, () => {
